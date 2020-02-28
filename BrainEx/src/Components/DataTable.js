ab1c7c8e831bdf6dbb778659a6cbf569c00cbb6d
mags1 <magoodwin@wpi.edu>
2020-02-28 18:43:43 -0500

    removed unused tablecell

--- BrainEx/src/Components/DataTable.js
+++ BrainEx/src/Components/DataTable.js
@@ -147,7 +147,6 @@ export default class DataTable extends Component {
                                           checkedIcon={<CheckBoxIcon style={{ fontSize: 20 }} />}
                                           onChange={this.selectAll}/>
                             </TableCell>
-                            {/*<TableCell>Color</TableCell>*/}
                             <TableCell>Rank</TableCell>
                             <TableCell>Sequence ID</TableCell>
                             <TableCell>Start Time</TableCell>
