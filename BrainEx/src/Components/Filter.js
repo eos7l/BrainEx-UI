5e440fda5d235a2a545043230be8edcdfbd74f5c
mags1 <magoodwin@wpi.edu>
2020-03-02 16:12:31 -0500

    clear query -> clear results

--- BrainEx/src/Components/Filter.js
+++ BrainEx/src/Components/Filter.js
@@ -246,7 +246,7 @@ export default function Filter(props) {
                                 Submit Query
                             </Button>
                             <Button size="medium" variant="contained" color="default" onClick={handleClearInput}>
-                                Clear Query {/*todo dialog letting user know that this will erase the table and graph*/}
+                                Clear Results {/*todo dialog letting user know that this will erase the table and graph*/}
                             </Button>
                         </ButtonGroup>
                     </div>
